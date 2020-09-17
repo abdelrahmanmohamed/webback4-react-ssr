0670bbd273f6429d7a981d3c46fd8538cf853523
abdelrahman.mohamed <abdelrahman.mohamed.sayed@gmail.com>
2020-09-17 12:52:24 +0200

    unable cors

--- src/js/server.js
+++ src/js/server.js
@@ -1,8 +1,8 @@
 /* eslint-disable react/jsx-filename-extension */
 import express from 'express';
 import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { ServerStyleSheet } from 'styled-components';
+import {renderToString} from 'react-dom/server';
+import {ServerStyleSheet} from 'styled-components';
 
 import App from './components/App';
 import Layout from './components/Layout';
@@ -10,37 +10,43 @@ import Layout from './components/Layout';
 const port = 3000;
 const server = express();
 
+const cors = require('cors');
+
+server.use(cors())
 server.use(express.static('dist'));
 server.use(express.static('images'));
 
 
 function renderComponent(component) {
-  const sheet = new ServerStyleSheet();
+    const sheet = new ServerStyleSheet();
 
-  const body = renderToString(sheet.collectStyles(component)); // get styles
-  const styles = sheet.getStyleTags(); // getting all the tags from the sheet
+    const body = renderToString(sheet.collectStyles(component)); // get styles
+    const styles = sheet.getStyleTags(); // getting all the tags from the sheet
 
-  return {
-    body,
-    styles, // passing the styles to our Layout template
-  };
+    return {
+        body,
+        styles, // passing the styles to our Layout template
+    };
 }
 
 // Creating a single index route to server our React application from.
 server.get('/aaa', (req, res) => {
-  const title = 'React Server Side Rendering with Styled Components';
-  const renderedComponent = renderComponent(<App />);
-
-  res.send(Layout({ title, ...renderedComponent }));
+    const title = 'React Server Side Rendering with Styled Components';
+    const renderedComponent = renderComponent(<App/>);
+    res.set('Content-Type', 'text/html')
+      .set('Access-Control-Allow-Origin', ' *')
+      .send(Layout({title, ...renderedComponent}));
 });
 
 server.get('/', (req, res) => {
-  const wrapDivStart = `<div id="app">`;
-  const body = renderToString(<App />);
-  const wrapDivEnd = `</div>`;
-  const hydrateScript= `<script src="client.js"></script>`;
-  const result = wrapDivStart+body+wrapDivEnd+hydrateScript;
-  res.send(result);
+    const wrapDivStart = `<div id="app">`;
+    const body = renderToString(<App/>);
+    const wrapDivEnd = `</div>`;
+    const hydrateScript = `<script src="client.js"></script>`;
+    const result = wrapDivStart + body + wrapDivEnd + hydrateScript;
+    res.set('Content-Type', 'text/html')
+        .set('Access-Control-Allow-Origin', ' *')
+        .send(result);
 });
 
 server.listen(port);
